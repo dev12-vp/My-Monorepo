cb1ba6b656e246a6f24a593803030dd854c027b3
Vivek <vivek@medianv.com>
2025-09-08 15:26:15 +0530

    Add nest monorepo

--- apps/post-service/src/app/app.module.ts
+++ apps/post-service/src/app/app.module.ts
@@ -5,17 +5,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { Post } from '../post/entity/post.entity';
 import { PostModule } from '../post/post.module';
+import * as dotenv from "dotenv";
+dotenv.config();
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }),
     PostModule,
   TypeOrmModule.forRoot({
     type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: '12321232',
-    database: 'postgres',
+    host: process.env.DB_HOST ,
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+    username: process.env.DB_USERNAME ,
+    password: process.env.DB_PASSWORD ,
+    database: process.env.DB_NAME ,
     entities: [Post],
     synchronize: true,
   }),
--- apps/user-service/src/app/app.module.ts
+++ apps/user-service/src/app/app.module.ts
@@ -5,17 +5,20 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '@my-workspace/common';
 import { UserModule } from '../user/user.module';
 import { ConfigModule } from '@nestjs/config';
+import * as dotenv from "dotenv";
+dotenv.config();
+
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }),
     UserModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '12321232',
-      database: 'postgres',
+      host: process.env.DB_HOST ,
+      port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+      username: process.env.DB_USERNAME ,
+      password: process.env.DB_PASSWORD ,
+      database: process.env.DB_NAME ,
       entities: [User],
       synchronize: true,
     }),
